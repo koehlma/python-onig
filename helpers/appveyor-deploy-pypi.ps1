4d6a12a466979e04541c3d8adb37a84d69fa839e
Maximilian Köhl <mkoehl@cs.uni-saarland.de>
2020-03-02 15:44:48 +0100

    bumb version

--- helpers/appveyor-deploy-pypi.ps1
+++ helpers/appveyor-deploy-pypi.ps1
@@ -1,5 +1,7 @@
-if ($env:APPVEYOR_REPO_TAG -eq 'true') {
-  Write-Output ("Deploying " + $env:APPVEYOR_REPO_TAG_NAME + " to PyPI...")
+Write-Output ("APPVEYOR_REPO_TAG: " + $Env:APPVEYOR_REPO_TAG)
+
+if ($Env:APPVEYOR_REPO_TAG -eq 'true') {
+  Write-Output ("Deploying " + $Env:APPVEYOR_REPO_TAG_NAME + " to PyPI...")
   & "${Env:PYTHON}/python.exe" -m pip install twine
   & "${Env:PYTHON}/python.exe" -m twine upload -u ${Env:PYPI_USER} -p ${Env:PYPI_PASS} --skip-existing dist/*.whl
 }
