6bf6fc2243b1c45beda5b7568d4f0e730da710f1
Maximilian Köhl <mkoehl@cs.uni-saarland.de>
2020-03-02 15:22:39 +0100

    appveyor deploy tag

---
+++ helpers/appveyor-deploy-pypi.ps1
@@ -0,0 +1,8 @@
+if ($env:APPVEYOR_REPO_TAG -eq 'true') {
+  Write-Output ("Deploying " + $env:APPVEYOR_REPO_TAG_NAME + " to PyPI...")
+  & "${Env:PYTHON}/python.exe" -m pip install twine
+  & "${Env:PYTHON}/python.exe" -m twine upload -u ${Env:PYPI_USER} -p ${Env:PY_PASS} --skip-existing dist/*.whl
+}
+else {
+  Write-Output "No tag for deployment"
+}
